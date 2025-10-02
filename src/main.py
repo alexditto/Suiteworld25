3aba4b7df8c19e55c72d6c22650c20977620a216
alexditto <alexdittocareer@gmail.com>
2025-10-02 16:43:41 -0500

    wip

--- src/main.py
+++ src/main.py
@@ -1,30 +1,7 @@
-from dotenv import load_dotenv
-import os
 import asyncio
 import requests
-from netsuite import NetSuite, Config, TokenAuth
 import analysis.data_parser as data_parser
-
-load_dotenv()
-
-config = Config(
-    account=os.getenv("NETSUITE_ACCOUNT"),
-    auth=TokenAuth(
-        consumer_key=os.getenv("NETSUITE_CONSUMER_KEY"),
-        consumer_secret=os.getenv("NETSUITE_CONSUMER_SECRET"),
-        token_id=os.getenv("NETSUITE_TOKEN"),
-        token_secret=os.getenv("NETSUITE_TOKEN_SECRET"),
-    )
-)
-ns = NetSuite(config)
-
-async def async_main():
-    # rest_api_results = await ns.rest_api.get("/record/v1/customer", params={"limit": 1})
-    # print(rest_api_results)
-    #
-    # restlet_results = await ns.restlet.get(2130, deploy=1)
-    restlet_results = await ns.restlet.post(2130, deploy=1, data={"internalId": 280375})
-    print(restlet_results)
+import netsuite_connection.connection as netsuite_connection
 
 
 if __name__ == "__main__":
@@ -32,6 +9,7 @@ if __name__ == "__main__":
     print(csv.head())
     print(data_parser.get_columns(csv))
     # data_parser.preprocess_data(csv, "csv/processed_train_inflows(in).csv")
-    # asyncio.run(async_main())
-
+    # asyncio.run(netsuite_connection.async_main())
+    # asyncio.run(netsuite_connection.async_restlet_get([280375, 280376, 280377]))
+    # asyncio.run(netsuite_connection.get_customer(4432102))
 
---
+++ src/netsuite_connection/connection.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+from netsuite import NetSuite, Config, TokenAuth
+
+load_dotenv()
+config = Config(
+    account=os.getenv("NETSUITE_ACCOUNT"),
+    auth=TokenAuth(
+        consumer_key=os.getenv("NETSUITE_CONSUMER_KEY"),
+        consumer_secret=os.getenv("NETSUITE_CONSUMER_SECRET"),
+        token_id=os.getenv("NETSUITE_TOKEN"),
+        token_secret=os.getenv("NETSUITE_TOKEN_SECRET"),
+    )
+)
+
+ns = NetSuite(config)
+
+async def async_main():
+    restlet_results = await ns.restlet.post(2130, deploy=1, data={"internalId": 280375})
+    print(restlet_results)
+
+async def async_restlet_get(customer_ids: list):
+    restlet_results = await ns.restlet.get(2130, deploy=1, params={"internal_ids": customer_ids})
+    print(restlet_results)
+
+async def async_restlet_post(customer_ids: list):
+    restlet_results = await ns.restlet.post(2130, deploy=1, data={"internal_ids": customer_ids})
+    print(restlet_results)
+
+async def get_customer(customer_id: int):
+    customer = await ns.rest_api.get("/record/v1/customer/" + str(customer_id))
+
+    print(customer)
